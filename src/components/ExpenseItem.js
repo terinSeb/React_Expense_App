02f939f092186686ee5652720cff0e7804bda96f
Terin Sebastin <terin01@gmail.com>
2022-09-30 07:17:21 +0100

    Added css file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import "./ExpenseItem.css";
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2022</div>
+      <div>
+        <div>Car Insurance</div>
+        <div>$259.98</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
